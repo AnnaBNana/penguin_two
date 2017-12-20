8161f2a37c24b6f554f0b935ebdf06b02974c824
Anna Propas <apropas@gmail.com>
2017-12-19 17:08:25 -0800

    removes mail test route

--- penguin/settings.py
+++ penguin/settings.py
@@ -29,7 +29,43 @@ ALLOWED_HOSTS = []
 
 INTERNAL_IPS = ('127.0.0.1')
 
-TEMPLATE_DEBUG = True
+# TEMPLATE_DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'applogfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'penguin.log'),
+            'maxBytes': 1024*1024*15, # 15MB
+            'backupCount': 10,
+        },
+    },
+
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        'penguin': {
+            'handlers': ['applogfile',],
+            'level': 'DEBUG',
+        },
+    }
+}
 
 # Application definition
 
